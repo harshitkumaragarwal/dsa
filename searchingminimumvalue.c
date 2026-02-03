0e531183801e965ce448ac320e324d98f7f491d4
harshitkumaragarwal <agarwalharshit063@gmail.com>
2026-02-03 23:25:51 +0530

    searching minimum value of array

---
+++ searchingminimumvalue.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+    int i;
+    int arr[5]={34,56,1,3,2};
+    int min=arr[0];
+
+    for(i=0;i<5;i++){
+        if(min>arr[i]){
+            min = arr[i];
+        }
+    }
+    printf("%d",min);
+
+    return 0;
+}
