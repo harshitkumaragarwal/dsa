2053ed8158a38b666a78251a95874483ce2746cc
harshitkumaragarwal <agarwalharshit063@gmail.com>
2026-02-07 10:51:23 +0530

    employee detail

---
+++ employeedetail.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include<time.h>
+
+struct Employee {
+    int id;
+    char name[30];
+    float salary;
+};
+
+int main() {
+    int n;
+    printf("Enter number of employees: ");
+    scanf("%d", &n);
+
+    struct Employee emp[n];
+    int i;
+    clock_t start, end;
+    double cpu_time_used;
+
+    start=clock();
+
+    for(i = 0; i < n; i++) {
+        printf("\nEnter details of employee %d\n", i + 1);
+
+        printf("ID: ");
+        scanf("%d", &emp[i].id);
+
+        printf("Name: ");
+        scanf(" %[^\n]", emp[i].name);
+
+        printf("Salary: ");
+        scanf("%f", &emp[i].salary);
+    }
+
+    printf("\n--- Employee Records ---\n");
+    for(i = 0; i < n; i++) {
+        printf("\nEmployee %d\n", i + 1);
+        printf("ID: %d\n", emp[i].id);
+        printf("Name: %s\n", emp[i].name);
+        printf("Salary: %.2f\n", emp[i].salary);
+    }
+
+    end = clock();
+
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    printf("CPU time used: %f seconds\n", cpu_time_used);
+
+    return 0;
+}
--- firstclass.c/clock1.c
+++ firstclass.c/clock1.c
@@ -18,12 +18,12 @@ int main() {
         }
     }
 
-    end = clock();
+    
+end = clock();
 
     cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
 
     printf("CPU time used: %f seconds\n", cpu_time_used);
-
     return 0;
 }
 
